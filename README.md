b6087293d7dba9b9409b807b1415f273cbd3b4f7
Jim Narey <jimnarey@me.com>
2024-02-01 21:25:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VSCode CMake Quickstart
+
+The product of following the VSCode CMake quickstart guide [here](https://code.visualstudio.com/docs/cpp/cmake-linux), to be used as a template for projects.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jimnarey/projects/cmake-quickstart
+# Build directory: /home/jimnarey/projects/cmake-quickstart/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
